29ddcc2beba7cb6f3ffa16d8a09c1b5595d5f123
PierreLouis98 <36404556+PierreLouis98@users.noreply.github.com>
2021-06-02 16:49:28 +0100

    ajout d'une fonction test multiply

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -1,11 +1,15 @@
 # test_addition.py
 
-from src.calculator import add
 import pytest
+from src.calculator import add, multiply
 
 def test_add():
     result = add(3, 4)
     assert result == 7
+
+def test_multiply():
+    result = multiply(3, 4)
+    assert result == 12
 
 def test_add_string():
     with pytest.raises(TypeError):
