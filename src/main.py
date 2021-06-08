65ebd776e557f25fc0e0dfb96f77dd692c000a16
PierreLouis98 <36404556+PierreLouis98@users.noreply.github.com>
2021-06-08 16:09:11 +0100

    print_hola()

--- src/main.py
+++ src/main.py
@@ -1,17 +1,15 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 from src.calculator import add
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {name}')
+
+def print_hola(name):
+    print(f'Holà, {name}')
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print_hola('Chouchou')
     print(add(3, 4))
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
