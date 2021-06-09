add3b73d40263f77351188eb328c87f8b5e46f86
PierreLouis98 <36404556+PierreLouis98@users.noreply.github.com>
2021-06-09 09:49:54 +0100

    Ajout fonction test

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -19,6 +19,10 @@ def test_subtract():
     result = subtract(4, 2)
     assert result == 2
 
+def test_divide_2():
+    result = divide(3, 2)
+    assert result == 1.5
+
 def test_add_string():
     with pytest.raises(TypeError):
         add("string", 4)
